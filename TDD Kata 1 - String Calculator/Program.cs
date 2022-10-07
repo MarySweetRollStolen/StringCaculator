571e333d3e06a91b0c04be9182b659bde6a034a0
MarySweetRollStolen <marylostherself.com>
2022-10-07 13:08:34 +0300

    Allow Add() hande string begins with delimiters

--- TDD Kata 1 - String Calculator/Program.cs
+++ TDD Kata 1 - String Calculator/Program.cs
@@ -7,13 +7,9 @@ namespace TDD_Kata_1___String_Calculator
         static void Main(string[] args)
         {
             StringCalculator ins = new StringCalculator();
-            int result = ins.Add("3, 7");
-            result = ins.Add("3, 7, 4");
-            result = ins.Add("1, 1, 1, 1, 1, 1");
-            result = ins.Add("1, 3");
-            result = ins.Add("3");
-            result = ins.Add("7");
-            result = ins.Add("");
+            int result = ins.Add("");
+            result = ins.Add("//;\n1;2");
+
 
             //Console.WriteLine(result);
 
