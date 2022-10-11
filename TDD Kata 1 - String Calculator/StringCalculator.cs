bae2af3b06e98b8aa8b1c0cd64fbcbcad359fb6a
MarySweetRollStolen <marylostherself.com>
2022-10-11 03:26:45 +0300

    Allow Add() throw excep with multiple negative num

--- TDD Kata 1 - String Calculator/StringCalculator.cs
+++ TDD Kata 1 - String Calculator/StringCalculator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TDD_Kata_1___String_Calculator
 {
@@ -14,8 +16,17 @@ namespace TDD_Kata_1___String_Calculator
             }
             if (numbers.Contains('-'))
             {
-                throw new ArgumentException(($"Negative numbers are not allowed (-{numbers[numbers.IndexOf('-') + 1]})").ToString());
+                Regex regex = new Regex(@"-\d+");
+                var negativeNumbersArray = regex.Matches(numbers).Cast<Match>().Select(m => m.Value).ToArray();
+                string negativeNumberForExceptionMessage = "";
+                foreach (string str in negativeNumbersArray)
+                {
+                    negativeNumberForExceptionMessage += str + ", ";
+                }
+                negativeNumberForExceptionMessage = negativeNumberForExceptionMessage.TrimEnd(',', ' ');
+                throw new ArgumentException(($"Negative numbers are not allowed (" + negativeNumberForExceptionMessage  + ")").ToString());
             }
+            
             int indexOfFistNumber = numbers.IndexOfAny("0123456789".ToCharArray());
             numbers = numbers.Substring(indexOfFistNumber, numbers.Length - indexOfFistNumber);
 
